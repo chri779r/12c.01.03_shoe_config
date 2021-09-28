0b09b210446602c9c1dbc72dd64fbac08718d324
CW Munch <chri779r@stud.kea.dk>
2021-09-28 22:39:41 +0200

    fetch working, shoe centered

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", fetchSvg);
+
+async function fetchSvg() {
+  console.log("DOM loaded and ready to fetch");
+  let response = await fetch("/shoe.svg");
+  console.log("response", response);
+  let mySvgData = await response.text();
+  console.log("SVG Data", mySvgData);
+  document.querySelector(".content").innerHTML = mySvgData;
+}
